1c0e0ffc08b83099e1aad2f1416ff1bdda969657
Caesar X Insanium <jesusramos.official@gmail.com>
2024-07-22 20:05:01 -0500

    managed to fix the segmentation fault, one more reason to use rust

--- lve/lve_pipeline.c
+++ lve/lve_pipeline.c
@@ -9,27 +9,28 @@ VkApplicationInfo vkappinfo(const char *name) {
   VkApplicationInfo result;
   result.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
   result.pApplicationName = name;
-  result.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  result.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
   result.pEngineName = "LveEngine";
-  result.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  result.engineVersion = VK_MAKE_VERSION(0, 0, 1);
   result.apiVersion = VK_API_VERSION_1_0;
+  result.pNext = NULL;
   return result;
 }
-VkInstanceCreateInfo vkinstanceinfo(const char *app_name) {
+VkInstanceCreateInfo vkinstanceinfo(VkApplicationInfo *info) {
   VkInstanceCreateInfo result;
-  memset(&result, 0,sizeof(VkInstance));
+  memset(&result, 0, sizeof(VkInstanceCreateInfo));
   result.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-  VkApplicationInfo info = vkappinfo(app_name);
-  result.pApplicationInfo = &info;
+  result.pApplicationInfo = info;
 
   uint32_t glfw_extension_count = 0;
   const char **glfw_extensions;
   glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
 
   result.enabledExtensionCount = glfw_extension_count;
-  result.ppEnabledLayerNames = glfw_extensions;
+  result.ppEnabledExtensionNames = glfw_extensions;
   result.enabledLayerCount = 0;
   result.ppEnabledLayerNames = NULL;
+  result.pNext = NULL;
   return result;
 }
 
@@ -37,11 +38,12 @@ LvePipeline *LvePipeline_new(const char *vert_path, const char *frag_path) {
   LvePipeline *pipeline = (LvePipeline *)malloc(sizeof(LvePipeline));
 
   // Creating Instance
-  VkInstanceCreateInfo createinfo = vkinstanceinfo("LveEngine");
+  VkApplicationInfo app_info = vkappinfo("LveEngine");
+  VkInstanceCreateInfo createinfo = vkinstanceinfo(&app_info);
   VkInstance instance;
   VkResult result = vkCreateInstance(&createinfo, NULL, &instance);
   PANIC_ASSERT("Failed to Create Instance.", result == VK_SUCCESS);
-
+  puts("Created Instance");
   // Load Shader Binary
   FILE *vert_fp = fopen(vert_path, "rb");
   FILE *frag_fp = fopen(frag_path, "rb");
